5ec27cdab64c80d2808138b814a281d8c56bbbf7
yeebdev27 <thoyyibmuhammad573@gmail.com>
2026-01-29 16:50:33 +0700

    Modifying Port

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -18,12 +18,12 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm start &
-sleep 1
+BROWSER=none HOST=0.0.0.0 PORT=3001 npm start &
+sleep 15
 echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
+echo 'Visit http://localhost:3001 to see your Node.js/React application in action.'
+echo '(This is why you specified the "args ''-p 3001:3001''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
